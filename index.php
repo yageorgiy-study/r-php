0e26e50797fd0e72ee6add1b47f8aa77722a0461
Yankovskiy Georgiy <admin@yageorgiy.ru>
2023-10-15 12:55:36 +0300

    Add class Task1

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
 <?php
 // TODO: sanitize input (?)
-$contents = file_get_contents("php://stdin");
-$jsonAssoc = json_decode($contents, true);
-$output = [];
 
-foreach($jsonAssoc as $item){
-    $output[$item["category"]][] = [
-        "id" => $item["id"]
-    ];
-}
+class Task1 {
 
-file_put_contents("./results.txt", json_encode($output));
+    public string $filename = "./results.txt";
+    public string $contents;
+
+    /**
+     * @param string $filename
+     * @param string $contents
+     */
+    public function __construct(string $filename, string $contents)
+    {
+        $this->filename = $filename;
+        $this->contents = $contents;
+    }
+
+
+    public function process(){
+        $contents = $this->contents;
+        $jsonAssoc = json_decode($contents, true);
+        $output = [];
+
+        foreach($jsonAssoc as $item){
+            $output[$item["category"]][] = [
+                "id" => $item["id"]
+            ];
+        }
+
+        file_put_contents($this->filename, json_encode($output));
+    }
+};
+
+(new Task1("./results.txt", file_get_contents("php://stdin")))->process();
